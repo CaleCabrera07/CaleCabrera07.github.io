3668fd1608db8c21c4f77953694cf9a9b6c0211c
Carlos Cabrera Semidey <ccarlosalejandros@gmail.com>
2020-12-22 22:27:09 -0500

    First draft, HTML done, CSS and responsive design done, folders done

---
+++ src/main.js
@@ -0,0 +1,7 @@
+/*SCROLL REVEAL*/
+ScrollReveal().reveal('.scroll');
+ScrollReveal().reveal('.About-Us', {delay: 300});
+ScrollReveal().reveal('.portafolio', {delay: 300});
+ScrollReveal().reveal('.clients', {delay: 300});
+ScrollReveal().reveal('.services', {delay: 300});
+ScrollReveal().reveal('.contact', {delay: 300});
