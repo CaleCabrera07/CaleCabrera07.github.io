a612b1629ac19c7d91b00ca5d5d1828f537e3b76
Carlos Cabrera Semidey <ccarlosalejandros@gmail.com>
2020-12-24 16:52:20 -0500

    correos php added

---
+++ src/correos.php
@@ -0,0 +1,15 @@
+<?php
+
+    $destinario = 'titinasemidey@gmail.com';
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    $header = "Enviado desde la página web de TITINASEMIDEY";
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+    mail($destinario, $mensajeCompleto, $header);
+    echo "<script> alert('Correo enviado exitosamente') </script>";
+    echo "<script> setTimeout(\"location.href='index.html'\", 1000) </script>";
+
+?>
